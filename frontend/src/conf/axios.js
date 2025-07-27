c2b6c913147895dabfde0e08cf77de4cd6e74057
Nitin-Anil-Narang <narangnitin520@gmail.com>
2025-07-27 14:13:17 +0530

    Change axios config.

--- frontend/src/conf/axios.js
+++ frontend/src/conf/axios.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const axiosApi = axios.create({
-    baseURL: import.meta.env.VITE_API_URL+'/api',
+    baseURL: import.meta.env.VITE_API_URL,
     // baseURL : "http://localhost:3000/api/",
+    // baseURL: import.meta.env.VITE_API_URL+'/api',
     withCredentials: true
 })
 export default axiosApi;
