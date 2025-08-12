06bdecbbc5741a6e8ad1ca6c60f1eb9ffd2d0777
Nitin-Anil-Narang <narangnitin520@gmail.com>
2025-08-12 23:33:53 +0530

    Add import reat properly as the previous team didnt add it

--- frontend/src/pages/NavPages/Events/components/HeroSection/HeroSection.jsx
+++ frontend/src/pages/NavPages/Events/components/HeroSection/HeroSection.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from "react";
+import React,{ useEffect, useState, useRef } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
