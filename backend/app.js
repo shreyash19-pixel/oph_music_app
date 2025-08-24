e50c61aa297c016160b0668d8f57fe90e4859897
Nitin-Anil-Narang <narangnitin520@gmail.com>
2025-08-25 00:21:30 +0530

    Update CORS configuration to include dynamic URLs from environment variables and mark tasks as completed in the TODO list.

--- backend/app.js
+++ backend/app.js
@@ -71,7 +71,7 @@ app.use(express.json());
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["http://localhost:5173", "http://localhost:5174",process.env.ADMIN_URL,process.env.CLIENT_URL],
     credentials: true,
   },
 });
