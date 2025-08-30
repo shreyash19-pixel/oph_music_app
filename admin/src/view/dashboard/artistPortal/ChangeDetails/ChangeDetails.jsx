40e153025bf0dda6ca071f97e5b1f926281934d5
bombaydc <bdcdev01@gmail.com>
2025-08-30 13:57:16 +0530

    fixed-video-details

--- admin/src/view/dashboard/artistPortal/ChangeDetails/ChangeDetails.jsx
+++ admin/src/view/dashboard/artistPortal/ChangeDetails/ChangeDetails.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axiosApi from "../../../../conf/axios";
+import SearchableDynamicTable from "../../../../components/SearchableDynamicTable";
+import ArtistSidebar from "../../../../components/ArtistSidebar";
 
 const ChangeDetails = () => {
-  return <div>ChangeDetails</div>;
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        console.log("calling");
+
+        const res = await axiosApi.get("/under-review-songs");
+        console.log("api success");
+
+        const formattedData = res.data.data.map((item) => {
+          return {
+            ...item,
+            // Format all ISO strings to "DD MMM YYYY" (like 27 Jul 2025)
+            release_date: item.release_date
+              ? new Date(item.release_date).toLocaleDateString("en-GB", {
+                  day: "2-digit",
+                  month: "short",
+                  year: "numeric",
+                })
+              : null,
+          };
+        });
+
+        setTableData(formattedData);
+        console.log(formattedData);
+      } catch (err) {
+        console.error("Error fetching data", err);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <ArtistSidebar>
+        <SearchableDynamicTable
+          title="Change Details"
+          data={tableData}
+          showStatusIndicator={false}
+          pageSize={10}
+          excludeColumns="availability_on_music_platform,current_page,status,reject_reason,Lyrics_services"
+          detailsUrl="/change-details"
+        />
+      </ArtistSidebar>
+    </div>
+  );
 };
 
 export default ChangeDetails;
