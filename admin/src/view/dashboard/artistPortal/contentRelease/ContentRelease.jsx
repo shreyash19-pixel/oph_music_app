da2f258a2d2b9b7cb0220e3bf24d4a8480467740
shreyash19-pixel <shreyashp734@gmail.com>
2025-09-07 01:14:04 +0530

    completed song details

---
+++ admin/src/view/dashboard/artistPortal/contentRelease/ContentRelease.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import axiosApi from "../../../../conf/axios";
+import SearchableDynamicTable from "../../../../components/SearchableDynamicTable";
+import ArtistSidebar from "../../../../components/ArtistSidebar";
+
+const ContentRelease = () => {
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axiosApi.get("/get-song-release-list");
+        console.log(res);
+        
+
+        setTableData(res.data.data);
+      } catch (err) {
+        console.error("Error fetching data", err);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <ArtistSidebar>
+        <SearchableDynamicTable
+          title="Song Release"
+          data={tableData}
+          showStatusIndicator={false}
+          pageSize={10}
+          detailsUrl="/ContentRelease"
+          excludeColumns={["youtube_release_time", "spotify_release_time", "apple_release_time", "instagram_release_time", "facebook_release_time", "share_url", "release_time"]}
+        />
+      </ArtistSidebar>
+    </div>
+  );
+};
+
+export default ContentRelease;
