ee3e1fab72fa305637651d4105f801ee7df977d5
shreyash19-pixel <shreyashp734@gmail.com>
2025-10-14 22:49:27 +0530

    updated

--- admin/src/view/dashboard/artistPortal/contentRelease/ContentRelease.jsx
+++ admin/src/view/dashboard/artistPortal/contentRelease/ContentRelease.jsx
@@ -9,9 +9,24 @@ const ContentRelease = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await axiosApi.get("/get-song-release-list");
-        console.log(res);
-        
+        let res = await axiosApi.get("/get-song-release-list");
+
+        res = res.data.data.map((item) => {
+          if ("release_date" in item) {
+            return {
+              ...item,
+              release_date: item.release_date
+                ? new Date(item.release_time).toLocaleDateString("en-GB", {
+                    day: "2-digit",
+                    month: "short",
+                    year: "numeric",
+                  })
+                : "N/A",
+            };
+          }
+          // If the key doesn't exist, return item as is
+          return item;
+        });
 
         setTableData(res.data.data);
       } catch (err) {
@@ -31,7 +46,20 @@ const ContentRelease = () => {
           showStatusIndicator={false}
           pageSize={10}
           detailsUrl="/ContentRelease"
-          excludeColumns={["youtube_release_time", "spotify_release_time", "apple_release_time", "instagram_release_time", "facebook_release_time", "share_url", "release_time", "youtube_url", "spotify_url", "apple_url", "instagram_url", "facebook_url"]}
+          excludeColumns={[
+            "youtube_release_time",
+            "spotify_release_time",
+            "apple_release_time",
+            "instagram_release_time",
+            "facebook_release_time",
+            "share_url",
+            "release_time",
+            "youtube_url",
+            "spotify_url",
+            "apple_url",
+            "instagram_url",
+            "facebook_url",
+          ]}
         />
       </ArtistSidebar>
     </div>
