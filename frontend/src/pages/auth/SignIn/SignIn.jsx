37b7f5b482b27793e4af9db5d4eae1d8d9399cb8
shreyash19-pixel <shreyashp734@gmail.com>
2025-10-30 23:05:26 +0530

    fixed-login-issue

--- frontend/src/pages/auth/SignIn/SignIn.jsx
+++ frontend/src/pages/auth/SignIn/SignIn.jsx
@@ -1,25 +1,7 @@
-
 import React, { useEffect, useState } from "react";
 import SignInForm from "./components/SignInForm/SignInForm";
-import { useArtist } from "../API/ArtistContext";
-import { useNavigate } from "react-router-dom";
 
 function SignIn() {
-    const [verified,setVerified] = useState(false);
-
-  const {headers} = useArtist();
-  const navigate = useNavigate();
-   useEffect(()=>{
-
-    if (!headers || !headers.Authorization) {
-      setVerified(false);
-    }
-    else{
-      setVerified(true);
-      navigate('/dashboard')
-    }
-
-   },[useArtist])
   return (
     <div>
       <SignInForm />
--- frontend/src/pages/auth/SignUp/SignUp.jsx
+++ frontend/src/pages/auth/SignUp/SignUp.jsx
@@ -1,24 +1,10 @@
-
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import SignUpForm from "./components/SignUpForm";
-import { useArtist } from "../API/ArtistContext";
-import { useNavigate } from "react-router-dom";
+
 
 function SignUp() {
-  const [verified,setVerified] = useState(false);
-  const {headers} = useArtist();
-  const navigate = useNavigate();
- useEffect(()=>{
 
-  if (!headers || !headers.Authorization) {
-    setVerified(false);
-  }
-  else{
-    setVerified(true);
-    navigate('/dashboard')
-  }
-
- },[useArtist])
   return (
     <div>
       <SignUpForm />
