9e18eb773626fb88240a50bb483af002dd16ca55
jitesh0311 <jiteshsalian1713@gmail.com>
2026-01-28 00:18:11 +0530

    Email Done for forgot pass

--- backend/model/reset_password.js
+++ backend/model/reset_password.js
@@ -3,7 +3,7 @@ const db = require('../DB/connect'); // MySQL connection
 
 const updatePassword = async (ophid, password) => {
 
-    const [rows] = await db.execute("UPDATE user_details SET user_pass = ? WHERE ophid = ?" ,[password, ophid])
+    const [rows] = await db.execute("UPDATE user_details SET user_pass = ? WHERE oph_id = ?" ,[password, ophid])
     return rows
 }
 
